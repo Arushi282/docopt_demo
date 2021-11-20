6c9082aa3b0ee8d4d9b45552c72c6fb51edba6e7
Arushi Ahuja <arushiahuja28@gmail.com>
2021-11-19 17:29:29 -0800

    Added print statment for arg 4

--- demo.py
+++ demo.py
@@ -3,7 +3,6 @@
 
 """This script prints out docopt args.
 Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
-
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
@@ -15,3 +14,4 @@ from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
+print(opt["--arg4"])
